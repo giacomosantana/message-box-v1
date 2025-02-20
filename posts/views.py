5246785c68e899cc8b50551dd15bf6596f85eec8
gsantana <giacomosantana@gmail.com>
2025-02-20 17:58:40 -0400

    publications list

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Publication
 
-# Create your views here.
+class PublicationList(ListView):
+    model = Publication
+    template_name = 'publications-list.html'
+    context_object_name = 'publications'
+    ordering = ['-date']
+    paginate_by = 5
